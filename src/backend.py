699404e9b0ad5712e501a7ede4ce7f3379b9dfdd
rreilman <r.reilman@st.hanze.nl>
2024-12-11 09:42:57 +0100

    Cleaning up a bit of the code
    Making it more readable

--- src/backend.py
+++ src/backend.py
@@ -15,13 +15,15 @@ def parse_config()-> configparser:
 
 def process_groups(config: configparser) -> pl.DataFrame:
     path = config.get("PATHS", "group_data")
-    
+
     barcodes_names: pl.dataframe = pl.read_csv(path)
 
     barcodes_names = barcodes_names.with_columns(controle_n = pl.int_range(pl.len()).over(" description")+1)
     barcodes_names = barcodes_names.with_columns(group_and_n = pl.concat_str([pl.col(' description'), pl.col("controle_n")]))
 
     barcodes_names = barcodes_names.with_columns(pl.col(pl.Utf8).str.strip_chars()).drop("controle_n")
+
+
     return barcodes_names
 
 
@@ -48,11 +50,12 @@ def read_data(config: configparser) -> pl.DataFrame:
             name_group: str = barcodes_names.filter(pl.col("barcode").cast(pl.String) == barcode_num[0]).select("group_and_n")
             temp_df: pl.DataFrame = temp_df.with_columns(pl.lit(name_group).alias("group_name"))
             resulting_df = pl.concat([temp_df, resulting_df])
-    
+
+
     return resulting_df
 
 
-def count_methylation_data(df):
+def count_methylation_data(df: pl.DataFrame) -> pl.DataFrame:
     all_groups = pl.DataFrame({"group_name": df["group_name"].unique()})
 
     return (df
@@ -66,14 +69,14 @@ def count_methylation_data(df):
 )
 
 
-def get_gene_info(annotated_bed, genes):
+def get_gene_info(annotated_bed: pl.DataFrame, genes: list[str]) -> pl.DataFrame:
     print("Getting gene info")
     df_wanted = (annotated_bed
                  .filter(pl.col("gene_name").is_in(genes)))
     return df_wanted
 
 
-def filter_genes(gene_list, df, annotated_bed):
+def filter_genes(gene_list: list[str], df: pl.DataFrame, annotated_bed: pl.DataFrame) -> pl.DataFrame:
     df_wanted = get_gene_info(annotated_bed, gene_list)
     final_subsetted_df: pd.DataFrame = pl.DataFrame(
         {"chr":[],
@@ -81,17 +84,18 @@ def filter_genes(gene_list, df, annotated_bed):
          "end":[],
          "frac":[],
          "valid":[],
-         "group_name":[]}
-    )
+         "group_name":[]})
+
     for row in df_wanted.iter_rows():
         (chromosome, promoter_start, promoter_end, gene_name) = row
         subsetted_df = df.filter(
             (pl.col("chr") == chromosome) &
             (pl.col("start") >= promoter_start) &
-            (pl.col("end") <= promoter_end)
-        )
+            (pl.col("end") <= promoter_end))
+
         final_subsetted_df = pl.concat([subsetted_df, final_subsetted_df])
-        
+
+
     return final_subsetted_df
 
 
@@ -104,20 +108,20 @@ def plot_barchart(df: pl.DataFrame) -> hvplot.plot:
                             xlabel = "Group name")
     return barplot
 
-def plot_scatter(df):
+def plot_scatter(df: pl.DataFrame) -> hvplot.plot:
     return df.hvplot.scatter(x = "start", y="chr", by="group_name", width = 750,
                              height = 400,
                              title = "Methylated DNA points",
                              xlabel = "Start positon of methylation",
                              ylabel = "Chromosome")
 
-def plot_density(df):
+def plot_density(df: pl.DataFrame) -> hvplot.plot:
     df = df.select(["group_name", "start"])
     return df.hvplot.kde(by="group_name", width = 750, height = 400,
                          title = "Density of methylation positions",
                          xlabel = "genomic positions")
 
-def plot_plots(df):
+def plot_plots(df: pl.DataFrame) -> pn.layout.Row[hvplot.plot]:
     barplot = plot_barchart(df)
     #scatter = plot_scatter(df)
     density = plot_density(df)
@@ -125,7 +129,7 @@ def plot_plots(df):
     
 
 
-def load_bed_file(config):
+def load_bed_file(config: configparser) -> pl.DataFrame:
     return pl.read_csv(config["PATHS"]["annotated_bed"], separator=",")
 
 
@@ -133,10 +137,10 @@ def filter_chr(chr_list: list[str], df: pl.DataFrame) -> pl.DataFrame:
     return (df.filter(pl.col("chr").is_in(chr_list)))
 
 
-def filter_group(group_list: list[str], df: pl.DataFrame):
+def filter_group(group_list: list[str], df: pl.DataFrame) -> pl.DataFrame:
     return (df.filter(pl.col("group_name").is_in(group_list)))
 
 
-def filter_ranges(min_range, max_range, df):
+def filter_ranges(min_range: int, max_range: int, df: pl.DataFrame) -> pl.DataFrame:
     return (df.filter((pl.col("start") >= min_range) &
                       (pl.col("end") <= max_range)))
--- src/ui.py
+++ src/ui.py
@@ -10,8 +10,10 @@ annotated_bed = be.load_bed_file(config=config)
 
 
 def create_settings():
-    chr_select = pn.widgets.MultiChoice(options = main_data["chr"].unique().to_list(), name = "chromosome:")
-    group_select = pn.widgets.MultiChoice(options = main_data["group_name"].unique().to_list(), name = "group:")
+    chr_select = pn.widgets.MultiChoice(options = main_data["chr"]
+                                        .unique().to_list(), name = "chromosome:")
+    group_select = pn.widgets.MultiChoice(options = main_data["group_name"]
+                                          .unique().to_list(), name = "group:")
 
 
     min_range = pn.widgets.IntInput(name = "range start")
@@ -19,7 +21,8 @@ def create_settings():
 
 
     #use_genes = pn.widgets.Checkbox(name = "use genes to search")
-    all_genes = pn.widgets.MultiChoice(options = annotated_bed["gene_name"].unique().to_list(), name = "genes:")
+    all_genes = pn.widgets.MultiChoice(options = annotated_bed["gene_name"]
+                                       .unique().to_list(), name = "genes:")
 
     return pn.layout.WidgetBox("# Settings", "### Configure settings for plotting",
                                chr_select, group_select,
@@ -31,15 +34,19 @@ def update_df(chr_select, group_select, min_range, max_range, all_genes):
     if chr_select:
         print("Filtering chromosome")
         filtered_data = be.filter_chr(chr_select, filtered_data)
+
     if group_select:
         print("Filter groups")
         filtered_data = be.filter_group(group_select, filtered_data)
+
     if min_range and max_range:
         print(min_range, max_range)
         filtered_data = be.filter_ranges(min_range, max_range, filtered_data)
+
     if all_genes:
         print("Filtering genes")
         filtered_data = be.filter_genes(all_genes, filtered_data, annotated_bed)
+
     print("Filtered data!")
     return filtered_data
 
@@ -55,13 +62,13 @@ def load_page():
 
     plots = pn.bind(be.plot_plots, final_df)
     print("Plotted!")
+
+
     return pn.template.MaterialTemplate(
         site = "Methylatie tijd",
         title = "gangang",
         sidebar = [settings_box],
-        main = [plots]
-    )
-
+        main = [plots])
 
 
 def main():
